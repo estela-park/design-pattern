3425efe5b48147e5d304a3f05e3b0fde9e0b7fe0
estela-park <stellapark401@naver.com>
2022-05-08 10:26:14 +0900

    edit

--- composite/Main.java
+++ composite/Main.java
@@ -5,16 +5,18 @@ public static void main(String[] args) {
         new CompositeBox(
                 new VideoGame("1", 100)
                 // why 2 indentation?
+                // personal preference
         ),
         new CompositeBox(
-                    new CompositeBox(
-                            new Book("2", 200),
-                            new Book("3", 300)
-                    ),
-                    new VideoGame("4", 400),
-                    new VideoGame("5", 500)
+                new CompositeBox(
+                        new Book("2", 200),
+                        new Book("3", 300)
+                ),
+                new VideoGame("4", 400),
+                new VideoGame("5", 500)
         )
     );
 
     DeliveryService.calculateOrderPrice();
+    // 1500$
 }
